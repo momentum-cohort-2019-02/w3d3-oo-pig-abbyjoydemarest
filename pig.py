4a3081c91a4c215a2c35cd55893cad4b6fc7b1dd
Abby Demarest <abbyjoydemarest@gmail.com>
2019-02-28 00:23:51 -0500

    updated documents. Added classes to pig.py

---
+++ pig.py
@@ -0,0 +1,51 @@
+class Game:
+
+
+    def __init__(self,):
+
+    
+    def __start__(self,):
+
+    def __end__(self,
+    scorce <+ 100
+
+
+    def __sum__(self, total):
+        self.total = total
+        total += self.total
+        return total
+
+
+ class HumanPlayer:
+
+
+     def __init__(self, name):
+     self.name = name
+    
+
+    def __roll__(self):
+        return random.randint(1:7)
+    
+
+    def __choose__(self):
+        if self.game.hold == 'yes':
+            return hold
+        else:
+            return roll
+
+
+class ComputerPlayer:
+
+
+    def __init__(self,name):
+        self.name = name
+
+
+    def roll_the_die(self):
+        return random.randint(1:7)
+    
+    def __choose__(self):
+        if self.game.counter < 20:
+            return roll
+        else:
+            return hold
